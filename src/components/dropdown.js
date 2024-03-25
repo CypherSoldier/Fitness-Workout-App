06855d994a45e80fd22db44b4a5c8489f919e280
CypherSoldier <calebwagner200@gmail.com>
2024-03-25 17:34:06 +0200

    first commit

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -11,7 +11,7 @@ function DropDown() {
   return (
       <div className="dropdown">
       <button className="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" id="close-CSS">
-        <img src="https://img.freepik.com/premium-photo/future-possibilities-futuristic-cyborg-human-modified-with-digital-technology-generative-ai_549702-454.jpg"
+        <img alt="profile" src="https://img.freepik.com/premium-photo/future-possibilities-futuristic-cyborg-human-modified-with-digital-technology-generative-ai_549702-454.jpg"
         className="rounded-circle"></img>
       </button>
       <ul className="dropdown-menu">

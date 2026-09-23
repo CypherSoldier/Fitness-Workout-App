8c77fb596b9cbae989290d2954a9ccb4b72886db
Caleb Wagner <calebwagner200@gmail.com>
2026-09-23 12:26:27 +0200

    removed Navbar component from layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,4 @@
-import NavBar from "../Navbar";
 import { Outlet } from "react-router-dom";
-
 export default function Layout() {
-  return (
-    <>
-      <NavBar />
-      <div className="page-content">
-        <Outlet />
-      </div>
-    </>
-  );
+  return <Outlet />;
 }
